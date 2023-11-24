63cf2373936aa37fe34323c9b23cb74a0e564440
Panagiotis Kousidis <panos4266@gmail.com>
2023-11-24 15:15:18 +0200

    init commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+#oh-my-posh init --config "C:\Users\Panos\Documents\Posh\my_powerlevel10k_lean.omp.json" pwsh | Invoke-Expression
+oh-my-posh init --config "C:\Users\panos\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
+
+Set-Alias vim nvim
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+
+
