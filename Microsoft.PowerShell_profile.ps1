bea4ae10867cb2045e3ad7bb1cf50e1cb428fe52
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-15 15:59:05 +0200

    add windowsTerminal settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 oh-my-posh init --config "$env:USERPROFILE\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
 
 Set-Alias vim nvim
@@ -5,3 +6,13 @@ Set-Alias vim nvim
 Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
 
 
+=======
+oh-my-posh init --config "C:\Users\panos\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
+
+Set-Alias vim nvim
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function NextHistory
+
+>>>>>>> f057116 (add windowsTerminal settings)
