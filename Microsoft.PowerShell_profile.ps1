b277a7562d114323bd498bb68f414456499e954e
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-15 16:34:06 +0200

    fixed pwsh setting

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 oh-my-posh init --config "$env:USERPROFILE\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
-
-Set-Alias vim nvim
-
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
-
-
-=======
-oh-my-posh init --config "C:\Users\panos\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
 
 Set-Alias vim nvim
 
@@ -15,4 +6,3 @@ Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
 Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function PreviousHistory
 Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function NextHistory
 
->>>>>>> f057116 (add windowsTerminal settings)
