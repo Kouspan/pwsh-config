1e0980e5802fbe3a1ff01defb8738aad595d4443
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-16 22:07:33 +0200

    more keybinds

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,9 @@ oh-my-posh init --config "$env:USERPROFILE\Documents\Powershell\Posh\my_pure.omp
 
 Set-Alias vim nvim
 
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function AcceptNextSuggestionWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+d" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord "Ctrl+s" -Function NextSuggestion
+Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function PreviousSuggestion
 Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function PreviousHistory
 Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function NextHistory
-
