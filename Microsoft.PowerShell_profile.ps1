734f5db85dcab93a3b8992aeacd3dd6f0b0899f4
Panagiotis Kousidis <panos4266@gmail.com>
2025-01-09 00:44:38 +0200

    udpate pure version

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 oh-my-posh init --config "$env:USERPROFILE\Documents\Powershell\Posh\my_pure.omp.json" pwsh | Invoke-Expression
 
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 Set-Alias vim nvim
 
 Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function AcceptNextSuggestionWord
